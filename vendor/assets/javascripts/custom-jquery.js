ef69374378cc06ad9ac4af3891c5d023f541db62
roy <roy.delima@teknion.com>
2014-10-23 08:59:17 -0400

    init

--- vendor/assets/javascripts/custom-jquery.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-        $('ul.nav > li').click(function (e) {
-            e.preventDefault();
-            $('ul.nav > li').removeClass('active');
-            $(this).addClass('active');   
-            $('#claim_content').load('backcharge.html');             
-        });            
-    });
-
