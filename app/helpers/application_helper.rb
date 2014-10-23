84c8f3c199912bca2daae323242b2caa9ad1b42e
roy <roy.delima@teknion.com>
2014-10-23 13:58:10 -0400

    init

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,28 +27,27 @@ module ApplicationHelper
 				@main_menu.push(@temp_menu_item)
 				
 			end
-    	 end
-    	 
-    	 def getAccounts(userGroups)
-    	 	 
-    	 	 @groups=Array.new
-    	 	 @dealerAccounts=Array.new
-    	 	 @groups=userGroups
-    	 	 @dealerGroups=@groups.grep(/all/)
-    	 	 @dealerGroups.each do |group_Name|
-    	 	 	 @tempGroup=group_Name.split('|')
-    	 	 	 @temp_dealer_group=@tempGroup[0].split('.')
-    	 	 	 dealer_account="#{@tempGroup[1]}|#{@temp_dealer_group[1]}"
-    	 	 	 @dealerAccounts.push(dealer_account)
-    	 	 end
-    	 	 
-    	 	 return @dealerAccounts
-    	 end
-    		
+		end
+	end
+	
+	def getAccounts(userGroups)
+		
+		@groups=Array.new
+		@dealerAccounts=Array.new
+		@groups=userGroups
+		@dealerGroups=@groups.grep(/all/)
+		@dealerGroups.each do |group_Name|
+			@tempGroup=group_Name.split('|')
+			@temp_dealer_group=@tempGroup[0].split('.')
+			dealer_account="#{@tempGroup[1]}|#{@temp_dealer_group[1]}"
+			@dealerAccounts.push(dealer_account)
+		end
+		
+		return @dealerAccounts
+	end
+	
 end
-    	 
-    	
-    	     		 
-    	     	    
- end
-end
+
+
+
+
