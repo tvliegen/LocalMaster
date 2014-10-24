315f08ee72e95d45d12bfda291761e889269497d
roy <roy.delima@teknion.com>
2014-10-24 09:09:08 -0400

    init

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Claim.create!([
+  {tekcare_number: Faker::Number.number(5), title: "Broken Spokes", project: "Wheelbuilder", created_by: Faker::Name.name, status: "in_progress"},
+  {tekcare_number: Faker::Number.number(5), title: "Cracked Carbon", project: "Framebuilder", created_by: Faker::Name.name, status: "in_progress"},
+  {tekcare_number: Faker::Number.number(5), title: "Crank stripped", project: "Welding", created_by: Faker::Name.name, status: "in_progress"},
+  {tekcare_number: Faker::Number.number(5), title: "Broken Hub", project: "Wheelbuilder", created_by: Faker::Name.name, status: "in_progress"},
+  {tekcare_number: Faker::Number.number(5), title: "Cracked Light", project: "Accessories", created_by: Faker::Name.name, status: "in_progress"},
+
+  {tekcare_number: Faker::Number.number(5), title: "Wrong Seat", project: "Accessories", created_by: Faker::Name.name, status: "clarification"},
+  {tekcare_number: Faker::Number.number(5), title: "New Bar Tape", project: "Accessories", created_by: Faker::Name.name, status: "clarification"},
+
+  {tekcare_number: Faker::Number.number(5), title: "Tire puncture", project: "Accessories", created_by: Faker::Name.name, status: "resolved"},
+  {tekcare_number: Faker::Number.number(5), title: "Seat post", project: "Accessories", created_by: Faker::Name.name, status: "resolved"},
+  {tekcare_number: Faker::Number.number(5), title: "New Pedals", project: "Accessories", created_by: Faker::Name.name, status: "resolved"}
+])
