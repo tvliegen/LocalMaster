2cb108f6db13c9098c6bbcd6ef29b6f75549fe48
roy <roy.delima@teknion.com>
2014-10-29 13:25:51 -0400

    125

--- db/migrate/20141016141441_add_password_to_usermanagement_users.rb
+++ db/migrate/20141016141441_add_password_to_usermanagement_users.rb
@@ -1,4 +1,4 @@
-class AddGroupsToUsermanagementUsers < ActiveRecord::Migration
+class AddPasswordToUsermanagementUsers < ActiveRecord::Migration
   def change
     add_column :usermanagement_users, :password, :string
     add_column :usermanagement_users, :password_confirm, :string
